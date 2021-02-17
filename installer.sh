6a7b537c38f23ae2a0abd081e186c9e487983140
Lorenzo Panetta <lorenzo.panetta@outlook.it>
2021-02-17 19:07:39 +0100

    fix unstaged files

--- installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp cpu-monitoring /usr/bin/cpu-monitoring
-sudo chmod +x /usr/bin/cpu-monitoring
-sudo cp cpu-monitoring.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl stop cpu-monitoring.service
-systemctl start cpu-monitoring.service
-systemctl status cpu-monitoring.service
