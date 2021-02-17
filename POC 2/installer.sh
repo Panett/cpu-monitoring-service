1a87e4f10596498fe8f0c83b10d125c4a53d47ba
Lorenzo Panetta <lorenzo.panetta@outlook.it>
2021-02-17 19:04:35 +0100

    add timer

---
+++ POC 2/installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp cpu-monitoring /usr/bin/cpu-monitoring
+sudo chmod +x /usr/bin/cpu-monitoring
+sudo cp cpu-monitoring.service /etc/systemd/system/
+sudo cp cpu-monitoring.timer /etc/systemd/system/
+
+systemctl daemon-reload
+systemctl stop cpu-monitoring.service
+systemctl start cpu-monitoring.service
+systemctl status cpu-monitoring.service
